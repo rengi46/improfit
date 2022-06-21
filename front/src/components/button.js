b33843a2a7796f4cc8a38707aef6ed6ab00a30e2
rogerpuig <rogerpuigdemasa@gmail.com>
2022-06-21 10:55:51 +0200

    icon button

--- front/src/components/button.js
+++ front/src/components/button.js
@@ -6,7 +6,9 @@ export const Button = ({goTo="/"}) => {
   return (
     <div className='w-full flex justify-start'>
       <button type="button" class=" text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-10 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-      onClick={()=>{navigate(goTo)}}>Back Home</button>
+      onClick={()=>{navigate(goTo)}}>
+       <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path></svg>
+      </button>
     </div>
   )
 }
