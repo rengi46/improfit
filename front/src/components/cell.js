9ed6b508809a85a3736b31cc89e50f009be3ca4c
rogerpuig <rogerpuigdemasa@gmail.com>
2022-06-21 10:50:00 +0200

    configuration dark mode

--- front/src/components/cell.js
+++ front/src/components/cell.js
@@ -7,7 +7,7 @@ export const Cell = ({rank,title,date}) => {
         navigate(`/${rank}`);
     }
   return (
-    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer hover:bg-gray-300" onClick={()=>{handleClick()}}>
+    <tr className=" border-b bg-gray-800 border-gray-700 cursor-pointer hover:bg-gray-600" onClick={()=>{handleClick()}}>
         <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
             {rank}
         </th>
